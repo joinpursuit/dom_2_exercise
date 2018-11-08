a34f312d91e3af457270396521c89974de00bf13
Andrew KIl <kil.andrew@gmail.com>
2018-11-08 15:36:11 -0500

    add features

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
-let timer = document.querySelector(".countdown");
+const timer = document.querySelector(".countdown");
 
-const countdown = num => {
+const timerCountdown = num => {
   const countD = setInterval(function() {
-    num--;
     timer.innerText = num;
+    if (num > 0) {
+      countD;
+    } else {
+      clearInterval(countD);
+    }
+    num--;
   }, 1000);
-  if (num >= 0) {
-    countD;
-  } else {
-    clearInterval(countD);
-  }
 };
-countdown(5);
+timerCountdown(5);
+
+// const timerCountdown = num => {
+//   const countD = setInterval(function() {
+//     num--;
+//     timer.innerText = num;
+//     if (num >= 0) {
+//       countD;
+//     } else {
+//       clearInterval(countD);
+//     }
+//   }, 1000);
+// };
+// timerCountdown(5);
+//
+// const jackCountdown = num => {
+//   const jackCountD = setInterval(function() {
+//     num--;
+//     timer.innerText = num;
+//     if (num >= 0) {
+//       countD;
+//     } else {
+//       clearInterval(countD);
+//       document.
+//     }
+//   }, 1000);
+// };
+//
+// const timer = document.querySelector(".countdown");
+// const countdown = num => {
+//   const countD = setInterval(function() {
+//     timer.innerText = num;
+//     if (num > 0) {
+//       countD;
+//     } else {
+//       clearInterval(countD);
+//     }
+//     num--;
+//   }, 1000);
+// };
+// countdown(5);
