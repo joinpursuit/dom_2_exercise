e80c2537dd2a5ef42e1f410172380297f59bdb51
Jonathan.e <jonathanerquinigo@pursuit.org>
2018-11-08 14:32:57 -0500

    WorkInProgress

---
+++ index.js
@@ -0,0 +1,5 @@
+//set up the setinterval here for the two pictures
+//you are going to use getElementsByClassName(all) thing to grab the elements needed
+//
+
+let jackInBox = document.querySelector("jack-hiding")
