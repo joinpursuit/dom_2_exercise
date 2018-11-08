f1ba50b658c1a5a8bd0c10e15b9ee11487ad0c27
Andrew KIl <kil.andrew@gmail.com>
2018-11-08 15:46:47 -0500

    get countdown timer to work

--- script.js
+++ script.js
@@ -7,48 +7,10 @@ const timerCountdown = num => {
       countD;
     } else {
       clearInterval(countD);
+      let jackHiding = document.getElementById("jack-hiding");
+      jackHiding.classList.add(".jack-surpise");
     }
     num--;
   }, 1000);
 };
 timerCountdown(5);
-
-// const timerCountdown = num => {
-//   const countD = setInterval(function() {
-//     num--;
-//     timer.innerText = num;
-//     if (num >= 0) {
-//       countD;
-//     } else {
-//       clearInterval(countD);
-//     }
-//   }, 1000);
-// };
-// timerCountdown(5);
-//
-// const jackCountdown = num => {
-//   const jackCountD = setInterval(function() {
-//     num--;
-//     timer.innerText = num;
-//     if (num >= 0) {
-//       countD;
-//     } else {
-//       clearInterval(countD);
-//       document.
-//     }
-//   }, 1000);
-// };
-//
-// const timer = document.querySelector(".countdown");
-// const countdown = num => {
-//   const countD = setInterval(function() {
-//     timer.innerText = num;
-//     if (num > 0) {
-//       countD;
-//     } else {
-//       clearInterval(countD);
-//     }
-//     num--;
-//   }, 1000);
-// };
-// countdown(5);
