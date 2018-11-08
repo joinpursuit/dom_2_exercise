987d1a55c7c2e3cacc3e03782d975fc21547f59b
Pixelynx <tjstevens1992@gmail.com>
2018-11-08 16:11:42 -0500

    finito

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+// countdown timer
+let countingDown = document.querySelector('.countdown');
+let counter = 5;
+
+const timer = setInterval(() => {
+
+  if (counter <= 0) {
+    clearInterval();
+    show_jack();
+  } else {
+    countingDown.innerHTML = counter;
+    counter--;
+  }
+
+  return counter;
+}, 1000)
+
+
+
 // DOM to change .jack-hiding img to new img
+
+const show_jack = () => {
+  let heresJacky = document.querySelector('.jack-hiding');
+  heresJacky.classList.remove('.jack-hiding');
+  heresJacky.classList.add('.jack-popping');
+  heresJacky.style.backgroundImage = "url('https://sep.yimg.com/ay/genius-babies/jester-jack-in-the-box-toy-25.gif')";
+
+}
