60cbd7c3618b5dc1e2e6980bfc1c23e04423f589
Erick <erickarellano@pursuit.org>
2018-11-09 01:50:47 -0500

    Erick Arellano

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ let change = document.querySelector(".jack-hiding")
 
 let time = setInterval(function() {
     change.classList.add("jack-pop");
-  }, 5000)
+}, 5000)
 
 let seconds = document.querySelector(".countdown")
 let counter = 5
 let timer = setInterval(function() {
-        counter--;
+  if (counter === 0){
+    return counter;
+  }
+    counter--;
     seconds.innerText=counter;
 }, 1000);
+
+// let seconds = document.querySelector(".countdown")
+// let counter = 5
+// let timer = setInterval(function() {
+//   if (counter === 0){
+//     return counter && seconds.classList.add("jack-pop");
+//   }
+//     counter--;
+//     seconds.innerText=counter;
+// }, 1000);
