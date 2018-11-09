cb57521dfea6697030d6e541d9f8290322f4898c
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-08 20:30:05 -0500

    Eunice - office hours

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //countdown:--------
-theTime = document.querySelector(".countdown");
 
 let arr = [0,1,2,3,4]
 
-function countDown () {
-  theTime.innerText = arr.pop();
+function countDown (node) {
+  node.innerText = arr.pop();
 }
 
 
   let timer = setInterval(() => {
+    let theTime = document.querySelector(".countdown");
+
     if (arr.length) {
-      countDown();
+      countDown(theTime);
     }
     // console.log(arr);
     else {
@@ -23,13 +24,62 @@ function countDown () {
 
 
 // //jack:--------
-theNodes = document.querySelector(".jack-hiding");
-
 function changeImg(node) {
   node.classList.remove("jack-hiding")
   node.classList.add("jack-seen")
 }
+//-----------
+function changeMessage(node) {
+  node.innerHTML = "Here! Being silly :}"
+}
+//-----------
+
+function addCelebration(node) {
+  node.classList.remove("yay")
+  node.classList.add("yayShow")
+}
+
+
+//-------------switch tenery ----
+// ----Reed's youtube video------
+// theBigYay = document.querySelector(".yayShow")
+function resizeYay (node) {
+  if (node.classList.contains("yayShow")) {
+    console.log("i was called yayShow to bigYay");
+
+    node.classList.remove("yayShow")
+    node.classList.add("yayBig")
+  } else {
+    console.log("i was called bigYay to yayShow");
+
+    node.classList.remove("yayBig")
+    node.classList.add("yayShow")
+  }
+}
+
+
+
+
+
+function flickerYay(node) {
+  let timer = setInterval(() => {
+    // console.log("i was called");
+    resizeYay(node)
+  }, 2000)
+}
+
+//------------
 
 setTimeout(() => {
+  theNodes = document.querySelector(".jack-hiding");
+  theMessage = document.querySelector(".jack-message");
+  theYay = document.querySelector(".yay");
+  theYay2 =  document.querySelector(".yay2");
+  theBigYay = document.getElementById('yaySpace');
+
   changeImg(theNodes);
+  changeMessage(theMessage)
+  addCelebration(theYay)
+  addCelebration(theYay2)
+  flickerYay(theBigYay)
 },5000)
